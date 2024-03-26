d6b99774f4eb714c8cc390855177340ce9ba7d42
Scott Pignataro <81174857+signatoro@users.noreply.github.com>
2024-03-26 03:12:56 -0400

    format code and update workflow

--- src/main.py
+++ src/main.py
@@ -1,4 +1,40 @@
+"""
+This module contains the implementation of a Minecraft Server Controller.
 
+The Minecraft Server Controller is responsible for managing a Minecraft server
+running in a Docker container. It provides methods for starting, stopping, and
+restarting the server, as well as performing backups and sending commands to the server.
+
+Classes:
+- McServerController: A class representing a Minecraft Server Controller.
+
+Attributes:
+- name (str): The name of the Minecraft server container.
+- max_ram (str): The maximum amount of RAM allocated to the server.
+- port (int): The port number for the server.
+- rcon (str): The RCON password for the server.
+- volumes (str): The path to the server data volume.
+- hardcore (bool): Whether hardcore mode is enabled.
+- difficulty (str): The difficulty level of the server.
+- version (str): The version of Minecraft server to run.
+- container (Container): The Docker container object representing the Minecraft server.
+- server_running (bool): Indicates whether the server is currently running.
+- client (DockerClient): The Docker client object for interacting with Docker.
+- last_restart_time (float): The timestamp of the last server restart.
+
+Methods:
+- __init__(self, name, max_ram, port, rcon, volumes, hardcore, difficulty, version): 
+    Initializes the Minecraft Server Controller.
+- run(self): Starts the server monitor loop.
+- start_docker_container(self): Starts the Docker container for the Minecraft server.
+- create_docker_container(self): Creates a new Docker container for the Minecraft server.
+- restart_server(self): Restarts the Minecraft server.
+- shutdown_server(self): Shuts down the Minecraft server.
+- backup_server_folder(self): Performs a backup of the server folder.
+- send_command(self, command): Sends a command to the Minecraft server via RCON.
+- __check_server_online(self): Checks if the server is online and ready to accept commands.
+- __await_status(self, status): Waits for the container status to reach the specified status.
+"""
 import time
 import shutil
 import logging
@@ -93,13 +129,31 @@ class McServerController:
 
             current_time = time.time()
 
-            logging.debug(f'current time: {current_time} last restart time: {self.last_restart_time}')
+            logging.debug(f'current time: {current_time} \
+                          last restart time: {self.last_restart_time}')
             if current_time - self.last_restart_time >= 24 * 60 * 60:
                 self.restart_server()
                 self.last_restart_time = current_time
 
 
     def start_docker_container(self):
+        """
+        Starts the Docker container for the Minecraft server.
+
+        This method checks if the container already exists. If it does, 
+        it checks the status of the container.
+        If the container is already running, it sets the `server_running` 
+        flag to True and calls the `run` method.
+        If the container is stopped, it starts the container, waits for the 
+        server to come online, and sets the `server_running` flag to True.
+        If the container has an unknown status, it raises an exception.
+
+        If the container does not exist, it creates a new Docker 
+        container using the `create_docker_container` method.
+
+        Returns:
+            None
+        """
         logging.info('Starting server')
 
         if self.client.containers.list(all=True, filters={'name': self.name}):
@@ -109,11 +163,11 @@ class McServerController:
                 logging.debug(f'Container {self.name} is stopped. starting...')
                 self.container.start()
                 self.__check_server_online()
-                self.server_running = True            
+                self.server_running = True
             elif self.container.status == 'running':
                 logging.debug(f'Container {self.name} is already running')
-                self.server_running = True 
-                self.run()  
+                self.server_running = True
+                self.run()
             else:
                 logging.error(f'!! Unknown Status !! Container status: {self.container.status}')
                 raise Exception(f'!! Unknown Status !! Container status: {self.container.status}')
@@ -123,10 +177,20 @@ class McServerController:
 
 
     def create_docker_container(self):
+        """
+        Creates a Docker container for running a Minecraft server with the specified parameters.
+
+        This method starts a Minecraft server using the itzg/minecraft-server Docker image.
+        It sets up the necessary environment variables, port mappings, and volumes for the server.
+        After starting the container, it checks if the server is online and logs the container ID.
+
+        Returns:
+            None
+        """
         logging.info('Starting Minecraft server with the following parameters:')
         logging.info(f'Max RAM: {self.max_ram}')
         logging.info(f'Port: {self.port}')
-        logging.info(f'RCON: {self.rcon}') 
+        logging.info(f'RCON: {self.rcon}')
         logging.info(f'Volumes: {self.volumes}')
         logging.info(f'Hardcore: {self.hardcore}')
         logging.info(f'Difficulty: {self.difficulty}')
@@ -161,6 +225,15 @@ class McServerController:
         self.server_running = True
 
     def restart_server(self):
+        """
+        Restarts the Minecraft server.
+
+        This method shuts down the server if it is running, backs up the server folder,
+        and then starts the Docker container.
+
+        If the server is not running, an error message is logged.
+
+        """
         logging.info("Restarting server Soon")
         if self.server_running:
             self.shutdown_server()
@@ -173,6 +246,19 @@ class McServerController:
 
 
     def shutdown_server(self):
+        """
+        Shuts down the Minecraft server gracefully.
+
+        This method sends a series of commands to the server to notify
+        players about the impending shutdown, waits for specific durations, and then 
+        stops the server container.
+
+        Note: The server_running attribute is set to False after the server 
+        is successfully shut down.
+
+        Returns:
+            None
+        """
         logging.debug("Stopping server")
         if self.server_running:
 
@@ -192,7 +278,7 @@ class McServerController:
 
             time.sleep(5)
 
-            self.send_command("say !! The server is being shut down !!") 
+            self.send_command("say !! The server is being shut down !!")
             self.send_command("say !! It will restart in a few moments !!")
 
             time.sleep(3)
@@ -201,7 +287,6 @@ class McServerController:
 
             self.container.stop()
 
-            # TODO: Check to see if this works.
             self.__await_status('exited')
 
             self.server_running = False
@@ -212,17 +297,42 @@ class McServerController:
 
 
     def backup_server_folder(self):
+        """
+        Backs up the server folder by creating a zip archive of the specified volumes.
+
+        This method uses the `shutil.make_archive` function to create a zip 
+        archive of the server folder.
+        The archive is named 'server_backup.zip' and is created in the 
+        same directory as the specified volumes.
+
+        Returns:
+            None
+        """
         logging.info("Backing up server folder")
         shutil.make_archive('server_backup', 'zip', self.volumes)
         logging.info("Backup complete")
 
 
     def send_command(self, command):
+        """
+        Sends a command to the Minecraft server using RCON.
+
+        Args:
+            command (str): The command to send to the server.
+
+        Returns:
+            str: The response from the server.
+
+        Raises:
+            Exception: If the command fails to execute.
+
+        """
         response = ''
 
         logging.debug(f'Sending command: {command}')
         try:
-            # Replace 'your_rcon_password' and 'your_minecraft_server_ip' with your actual RCON password and server IP
+            # Replace 'your_rcon_password' and 'your_minecraft_server_ip'
+            # with your actual RCON password and server IP
             with MCRcon('0.0.0.0', 'super', 25575, timeout=10) as client:
                 response = client.command(command)
                 logging.debug(f'Message Response: {response}')
@@ -247,6 +357,17 @@ class McServerController:
             self.container.reload()
 
 def set_up_logging(level_str, log_file_size=1):
+    """
+    Set up logging configuration.
+
+    Args:
+        level_str (str): The logging level as a string. Valid values are
+                        'DEBUG', 'CRITICAL', 'ERROR', 'WARNING', and 'INFO'.
+        log_file_size (int, optional): The maximum size of the log file in megabytes. Defaults to 1.
+
+    Returns:
+        None
+    """
     if level_str == 'DEBUG':
         logging_level = logging.DEBUG
     elif level_str == 'CRITICAL':
@@ -260,28 +381,15 @@ def set_up_logging(level_str, log_file_size=1):
 
     logging.basicConfig(level=logging_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # # Create a stream handler to print logs to the terminal
-    # stream_handler = logging.StreamHandler(sys.stdout)
-    # stream_handler.setLevel(logging_level)
-    # stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-
-    # # Add the stream handler to the root logger
-    # logging.getLogger().addHandler(stream_handler)
-
     # Add a rotating file handler to limit the log file size
     file_handler = RotatingFileHandler(
         'server.log',
         maxBytes=1024*1024*log_file_size,
         backupCount=5)
-    file_handler.setLevel(logging_level
-    )
+    file_handler.setLevel(logging_level)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
     logging.getLogger().addHandler(file_handler)
-
-    # Add the file handler to the root logger
-
-    # logging.basicConfig(level=logging_level)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Start Minecraft server with Docker')
